15c7f810fb589beb4c19f6030bde5c287ca78bb2
qiutao <mrqiutao@163.com>
2024-10-31 20:41:15 +0800

    update

--- socketHomeWork/src/Client.java
+++ socketHomeWork/src/Client.java
@@ -1,6 +1,8 @@
-import java.io.*;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+
+
 
 public class Client {
     public static void main(String[] args) throws IOException {
@@ -14,107 +16,31 @@ public class Client {
 
         //启动后首先建立连接，并提示登录
         Socket socket = new Socket("127.0.0.1",10000);//与服务器建立连接
+        System.out.println("服务器已经成功连接");
 
-        //需要循环
-
-        while (true) {
-            System.out.println("服务器已经连接成功");
-            System.out.println("==============欢迎来到黑马聊天室================");
-            System.out.println("1登录");
-            System.out.println("2注册");
-            System.out.println("请输入您的选择：");
-
+        //界面循环
+        while (true){
+            System.out.println("===============================================");
+            System.out.println("1.登陆");
+            System.out.println("2.注册");
+            System.out.println("请输入您的选择");
             Scanner sc = new Scanner(System.in);
-            int choice = sc.nextInt();
-            if (choice==1){
-                //登录
-                login(sc, socket);
-    
-            } else if (choice==2) {
-                //注册
-                register(sc, socket);
+            String choose = sc.nextLine();
+            //根据选项跳转
+            switch (choose){
+                case "1"-> login(socket);//登陆
+                case "2"-> register(socket);//注册
+                default -> System.out.println("输入错误");
+
             }
         }
     }
 
-    private static void register(Scanner sc, Socket socket) throws IOException {
-        //获取输出流
-        OutputStream os = socket.getOutputStream();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));//利用转换流
-        //将用户名和密码整行写出:1.bufferedriter写出字符串再换行；2.printwriter的writeline方法
-        getInputAndOut(sc, bw,"register");
+    private static void register(Socket socket) {
 
-        //接受信息：服务端的信息验证结果
-        InputStream inputStream = socket.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));//获取输入流
-        String message = br.readLine();
-        //根据验证结果输出
-        if (message.equals("1")) {
-            System.out.println("登陆成功");
-            //开启一个线程，并开始聊天
-            //对所有人说话（一直可以说话）
-            allwaysTalk2All(sc, bw);
-        } else if (message.equals("2")) {
-            System.out.println("密码错误");
-        } else if (message.equals("3")) {
-            System.out.println("用户名不存在");
-        }
     }
 
-    private static void allwaysTalk2All(Scanner sc, BufferedWriter bw) throws IOException {
-        while (true) {
-            //可以一直说话
-            System.out.println("请输入您想说的话");
-            String str = sc.nextLine();
-            //写出聊天的话到服务器
-            bw.write(str);
-            bw.newLine();
-            bw.flush();
-        }
-    }
+    private static void login(Socket socket) {
 
-    private static void getInputAndOut(Scanner sc, BufferedWriter bw,String choice) throws IOException {
-        System.out.println("请输入用户名：");
-        String usernameInput = sc.nextLine();
-        System.out.println("请输入密码：");
-        String passwordInput = sc.nextLine();
-        //按照指定格式发送给服务端,整行写出（bufferedWriter）
-
-        //写出登陆选项
-        bw.write(choice);
-        bw.newLine();
-        bw.flush();
-
-        //写出输入的信息
-        StringBuilder sb = new StringBuilder();//字符串拼接
-        sb.append("username=").append(usernameInput).append("&").append("password=").append(passwordInput);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
-        //br.close();//写出完毕，释放资源
-    }
-
-    private static void login(Scanner sc, Socket socket) throws IOException {
-        //获取输出流
-        OutputStream os = socket.getOutputStream();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));//利用转换流
-        //将用户名和密码整行写出:1.bufferedriter写出字符串再换行；2.printwriter的writeline方法
-        getInputAndOut(sc, bw,"login");
-
-        //接受信息：服务端的信息验证结果
-        InputStream inputStream = socket.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));//获取输入流
-        String message = br.readLine();
-        //根据验证结果输出
-        if (message.equals("1")){
-            System.out.println("注册成功成功");
-            //开启一个线程，并开始聊天
-            //对所有人说话（一直可以说话）
-            allwaysTalk2All(sc, bw);
-        } else if (message.equals("2")) {
-            System.out.println("用户名已存在");
-        } else if (message.equals("3")) {
-            System.out.println("用户名或密码格式错误");
-        }
     }
 }
