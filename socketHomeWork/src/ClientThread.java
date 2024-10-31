b55dcbefcfabb2a382c10981026bfcd622879b13
QiuTao <qiutao@MacBook-Air.local>
2024-10-31 18:28:05 +0800

    update

---
+++ socketHomeWork/src/ClientThread.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientThread implements Runnable{
+    //客户端线程：用于持续接受服务端转发的数据
+    Socket socket;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+//循环，重复的接受
+        while (true) {
+            try {
+                //接收服务器发送过来的聊天记录
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String msg = br.readLine();
+                System.out.println(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
